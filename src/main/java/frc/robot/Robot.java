2dcfe0c45af05e2c8fbf8925a97ba2e0811838f7
ajung26 <ajung26@csus.org>
2025-01-18 20:17:00 -0800

    Initial Commit 2

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,11 +2,19 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import java.io.File;
+import java.io.IOException;
+import swervelib.parser.SwerveParser;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -17,6 +25,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
+
+  private Timer disabledTimer;
 
   /**
    * This function is run when the robot is first started up and should be used for any
---
+++ src/main/java/frc/robot/commands/ElevatorL1Command.java
@@ -0,0 +1,3 @@
+public class ElevatorL1Command {
+    
+}
