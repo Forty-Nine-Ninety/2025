31ae68ef2b0656ade95489134608e1dd5b4314be
CrystalMilan <mkhedekar28@crystal.org>
2025-01-23 22:22:48 -0800

    Start of L1 Subsystem
    I started working on the L1 subsytem. Did not get far though

---
+++ src/main/java/frc/robot/subsystems/L1Subsystem.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+//Shreyans & Milan
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+
+public class L1Subsystem extends SubsystemBase {
+  private CANSparkMax intake;
+  /** Creates a new ExampleSubsystem. */
+  public L1Subsystem() {
+    intake = new CANSparkMax(Ports.CAN_INTAKE_SPARKMAX, MotorType.kBrushless);
+    configureMotors();
+  }
+
+
+  private void configureMotors(){
+    intake.restoreFactoryDefaults();
+    intake.setSmartCurrentLimit(TBD);
+  }
+
+  public void depositCoral (){
+
+  }
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
