fd75edd56f694cff000cb8512814e5f54baf6d9f
CrystalMilan <mkhedekar28@crystal.org>
2025-01-25 14:21:19 -0800

    Added L1ShooterSubsystem

--- src/main/java/frc/robot/subsystems/L1ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/L1ShooterSubsystem.java
@@ -1,47 +1,56 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class L1Subsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public L1Subsystem() {}
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
+
+public class L1ShooterSubsystem extends SubsystemBase {
+  private CANSparkMax shooter;
+
+  public L1Subsystem() {
+    outtakeMotor = new CANSparkMax(Ports.CAN_SHOOTER_SPARKMAX, MotorType.kBrushless);
+
+    pid_L1shooter = tbd.getPIDController();
+    pid_encoder = tbd.getEncoder();
+
+    configureMotors();
+  }
+
+
+  private void configureMotors(){
+
+    outtakeMotor.restoreFactoryDefaults();
+    outtakeMotor.setSmartCurrentLimit(60);
+
+    // outtakeMotor.setInverted(true);
+
+    pid_L1shooter.setP(MotionControl.L1SHOOTER_tbd_PID.kP);
+    pid_L1shooter.setI(MotionControl.L1Shooter_tbd_PID.kI);
+    pid_L1shooter.setD(MotionControl.L1Shooter_tbd_PID.kD);
+
+    outtakeMotor.setIdleMode(IdleMode.kBrake);
+    // tbd - maybe not
+
+    outtakeMotor.setClosedLoopRampRate(MotionControl.CLOSED_LOOP_RAMP_RATE);
+    outtakeMotor.setOpenLoopRampRate(MotionControl.OPEN_LOOP_RAMP_RATE);
+    
+  }
+
+  public void runMotor(double percent_output){
+    outtakeMotor.set(percent_output);
+  }
+
+  public void stop(){
+    outtakeMotor.set(0.0);
+  }
+
+  /* 
+  public void resetMotor(){
+  pid_encoder.setPosition(0);
+  }
    */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */`
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
 }
