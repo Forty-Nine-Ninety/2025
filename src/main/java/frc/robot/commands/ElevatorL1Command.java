330a5d167fc6d3d1a2386830a0774984a13924c9
aileen28 <ayang28@crystal.org>
2025-01-25 15:30:55 -0800

    Update ElevatorL1Command.java

--- src/main/java/frc/robot/commands/ElevatorL1Command.java
+++ src/main/java/frc/robot/commands/ElevatorL1Command.java
@@ -1,3 +1,17 @@
-public class ElevatorL1Command {
-    
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+
+public class ElevatorL1Command extends Command{
+    private final ElevatorSubsystem m_elevator;
+    public ArmAmpCommand(ElevatorSubsystem m_elevator){
+        m_elevator = elevator;
+        addRequirements(elevator);
+    }
+    @Override
+    public void initialize(){
+        elevator.moveToPosition(tbd);
+
+    }
 }
