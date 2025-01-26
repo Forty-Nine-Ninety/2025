db4c07432db2448758d1fffe1afe0744442ee5cd
aileen28 <ayang28@crystal.org>
2025-01-25 16:32:37 -0800

    Update ElevatorL3Command.java

---
+++ src/main/java/frc/robot/commands/ElevatorL3Command.java
@@ -0,0 +1,18 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+
+public class ElevatorL3Command extends Command{
+// instantiate the class so it transfers from elevator subsystem
+    private final ElevatorSubsystem m_elevator;
+    public ElevatorL3Command (ElevatorSubsystem m_elevator){
+        m_elevator = elevator;
+        addRequirements(elevator);
+    }
+    @Override
+    public void initialize(){
+        elevator.moveToPosition(tbd);
+
+    }
+}
