0da9304733bb443bc997aa86ee11bdc6b865b10e
ajung26 <ajung26@csus.org>
2025-01-25 20:08:31 -0800

    Mihika's ElevatorClimbCommand

--- src/main/java/frc/robot/commands/ElevatorClimbCommand.java
+++ src/main/java/frc/robot/commands/ElevatorClimbCommand.java
@@ -1,3 +1,21 @@
-public class DriveCommand {
-    
+//Mihika
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+public class ElevatorClimbCommand extends Command{
+    private final ElevatorSubsystem m_elevator;
+
+    public ElevatorClimbCommand(ElevatorSubsystem elevator){
+        m_elevator = elevator;
+        addRequirements(elevator);
+    }
+
+    @Override
+    public void initialize(){
+        m_elevator.moveToPosition(/*setPoint here*/);
+
+    }
 }
