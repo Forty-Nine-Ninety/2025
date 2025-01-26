3590893797625188870d409c7ef4c2ec91e32cb9
aileen28 <ayang28@crystal.org>
2025-01-25 16:34:33 -0800

    Update ElevatorL2Command.java

--- src/main/java/frc/robot/commands/ElevatorL2Command.java
+++ src/main/java/frc/robot/commands/ElevatorL2Command.java
@@ -1,43 +1,18 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
-
-import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
 
-/** An example command that uses an example subsystem. */
-public class ShootL2Command extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ShooterSubsystem m_subsystem;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ShootL2Command(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
-  }
+public class ElevatorL2Command extends Command{
+// instantiate the class so it transfers from elevator subsystem
+    private final ElevatorSubsystem m_elevator;
+    public ElevatorL2Command (ElevatorSubsystem m_elevator){
+        m_elevator = elevator;
+        addRequirements(elevator);
+    }
+    @Override
+    public void initialize(){
+        elevator.moveToPosition(tbd);
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    }
 }
