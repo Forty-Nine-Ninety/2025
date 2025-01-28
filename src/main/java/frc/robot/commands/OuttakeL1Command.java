31e1d55ab5d9dddec7cdc6c9031ea0e7690b50d8
Ario <ario.kerrigan@gmail.com>
2025-01-28 15:57:49 -0800

    added outake L1 command

---
+++ src/main/java/frc/robot/commands/OuttakeL1Command.java
@@ -0,0 +1,18 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.L1ShooterSubsystem;
+public class L1ShooterSubsystem extends Command{
+// instantiate the class so it transfers from elevator subsystem
+    private final L1ShooterSubsystem m_L1Shooter;
+    public ElevatorL1Command (L1ShooterSubsystem m_L1Shooter){
+        m_L1Shooter = L1Shooter;
+        addRequirements(L1Shooter);
+
+    }
+
+    @Override
+        public void initialize(){
+            L1Shooter.runMotor(1);
+    }
+
+}
