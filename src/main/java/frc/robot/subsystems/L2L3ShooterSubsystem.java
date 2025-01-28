1f21a49d6122e7f4ccb5d9259139fac63d4b412d
chloe295 <117236061+chloe295@users.noreply.github.com>
2025-01-28 15:35:52 -0800

    L2L3Shooter
    Initial commit

--- src/main/java/frc/robot/subsystems/L2L3ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/L2L3ShooterSubsystem.java
@@ -1,47 +1,43 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import frc.robot.Constants.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public class ShooterSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ShooterSubsystem() {}
+public class L2L3ShooterSubsystem extends SubsystemBase {
+  
+  private CANSparkMax right, left; 
+  private SparkPIDController shooterPID;
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  public L2L3ShooterSubsystem() { // Constructor 
+    right = CANSparkMax(Ports.CAN_L2L3_SHOOTER_RIGHT, MotorType.kBrushless)
+    left = CANSparkMax(Ports.CAN_L2L3_SHOOTER_LEFT, MotorType.kBrushless)
   }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
+}
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  private void configureMotors() {
+    right.restoreFactoryDefaults(); 
+    left.restoreFactoryDefaults();
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+    right.setInverted() = true;
+
+    right.setSmartCurrentLimit();
+    left.setSmartCurrentLimit();
+
+    shooterPID.setP(L2L3_SHOOTER_PID); // PID values not in yet 
+    shooterPID.setI(L2L3_SHOOTER_PID);
+    shooterPID.setD(L2L3_SHOOTER_PID);
+
+    right.setClosedLoopRampRate(MotionControl.CLOSED_LOOP_RAMP_RATE);
+    left.setClosedLoopRampRate(MotionControl.CLOSED_LOOP_RAMP_RATE);
+    right.setOpenLoopRampRate(MotionControl.OPEN_LOOP_RAMP_RATE);
+    left.setOpenLoopRampRate(MotionControl.OPEN_LOOP_RAMP_RATE);
 }
