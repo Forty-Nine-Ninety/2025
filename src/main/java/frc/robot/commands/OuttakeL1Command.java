d50996712ff85abeaafbb045276af2b0ac1bed9c
Ario <ario.kerrigan@gmail.com>
2025-01-31 15:58:32 -0800

    Fixed L1command

--- src/main/java/frc/robot/commands/OuttakeL1Command.java
+++ src/main/java/frc/robot/commands/OuttakeL1Command.java
@@ -1,15 +1,15 @@
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.L1ShooterSubsystem;
-public class L1ShooterSubsystem extends Command{
+public class OuttakeL1Command extends Command{
 // instantiate the class so it transfers from elevator subsystem
     private final L1ShooterSubsystem m_L1Shooter;
-    public ElevatorL1Command (L1ShooterSubsystem m_L1Shooter){
-        m_L1Shooter = L1Shooter;
-        addRequirements(L1Shooter);
+    public OutakeL1Command (L1ShooterSubsystem L1ShooterSubsystem){
+        m_L1Shooter = L1ShooterSubsytem;
+        addRequirements(L1ShooterSubsytem);
     }
     @Override
         public void initialize(){
-            L1Shooter.runMotor(1);
+            m_L1Shooter.runMotor(1);
     }
 }
