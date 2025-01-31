e2a3274c842070d3ec5115044da16fd083aaf619
Shreyans J. <145509892+shreyanscrystal@users.noreply.github.com>
2025-01-30 16:11:58 -0800

    Fix!
    Fixed OuttakeL1 command

--- src/main/java/frc/robot/commands/OuttakeL1Command.java
+++ src/main/java/frc/robot/commands/OuttakeL1Command.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
-<<<<<<< Updated upstream
 import frc.robot.subsystems.L1ShooterSubsystem;
 public class L1ShooterSubsystem extends Command{
 // instantiate the class so it transfers from elevator subsystem
@@ -8,29 +7,9 @@ public class L1ShooterSubsystem extends Command{
     public ElevatorL1Command (L1ShooterSubsystem m_L1Shooter){
         m_L1Shooter = L1Shooter;
         addRequirements(L1Shooter);
-
     }
-
     @Override
         public void initialize(){
             L1Shooter.runMotor(1);
     }
-
-=======
-import frc.robot.subsystems.ElevatorSubsystem;
-
-
-public class ElevatorL1Command extends Command{
-// instantiate the class so it transfers from elevator subsystem
-    private final ElevatorSubsystem m_elevator;
-    public ElevatorL1Command (ElevatorSubsystem m_elevator){
-        m_elevator = elevator;
-        addRequirements(elevator);
-    }
-    @Override
-    public void initialize(){
-        elevator.moveToPosition(tbd);
-
-    }
->>>>>>> Stashed changes
 }
