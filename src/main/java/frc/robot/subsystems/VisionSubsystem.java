847e00cc97b1ba4382751ac191200a6ecbfce661
Shreyans J. <145509892+shreyanscrystal@users.noreply.github.com>
2025-02-04 15:41:51 -0800

    Created VisionSubsystem

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,3 +1,22 @@
+import edu.wpi.first.photonvision.PhotonCamera;
+import edu.wpi.first.photonvision.PhotonPipelineResult;
+import edu.wpi.first.photonvision.PhotonPoseEstimator;
+
 public class VisionSubsystem {
-    
+    private PhotonCamera arducamOne;
+    // private PhotonCamera arducamTwo;
+    private PhotonPoseEstimator poseEstimator;
+
+    public VisionSubsystem(){
+        arducamOne = new PhotonCamera("arducamOne");
+        // arducammTwo = new PhotonCamera("arducamTwo");
+    }
+
+    public void update(){
+        PhotonPipelineResult result = camera.getLatestResult();
+
+        if(result.hasTargets()) {
+            poseEstimator.update();
+        }
+    }
 }
