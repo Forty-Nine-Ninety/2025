d9cba9011f49260c4ccfe621321e9fd2cca6a7f2
Shreyans J. <145509892+shreyanscrystal@users.noreply.github.com>
2025-02-04 14:42:57 -0800

    Update ElevatorSubsystem.java

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -9,7 +9,6 @@ import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import frc.robot.Constants.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
