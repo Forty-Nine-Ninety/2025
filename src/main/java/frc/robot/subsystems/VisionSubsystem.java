5e80dd0e72e7ec0196430800557daee7b5961d84
ajung26 <ajung26@csus.org>
2025-02-08 16:11:14 -0800

    Vision

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,22 +1,39 @@
+package frc.robot.subsystems;
+
 import edu.wpi.first.photonvision.PhotonCamera;
 import edu.wpi.first.photonvision.PhotonPipelineResult;
 import edu.wpi.first.photonvision.PhotonPoseEstimator;
+import frc.robot.subsystems.SwerveSubsystem;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class VisionSubsystem {
+    private SwerveSubsystem m_drivebase;
     private PhotonCamera arducamOne;
-    // private PhotonCamera arducamTwo;
-    private PhotonPoseEstimator poseEstimator;
+    //private PhotonPoseEstimator poseEstimator;
+    private PhotonPipelineResult result;
+    private PhotonTrackedTarget currentTarget;
+    private Transform2D pose;
+    int aprilTagiD;
 
-    public VisionSubsystem(){
-        arducamOne = new PhotonCamera("arducamOne");
-        // arducammTwo = new PhotonCamera("arducamTwo");
+    public VisionSubsystem(SwerveSubsystem drivebase){
+        m_drivebase = new SwerveSubsystem(drivebase);
+        arducamOne = new PhotonCamera("Arducam 1");
     }
 
     public void update(){
-        PhotonPipelineResult result = camera.getLatestResult();
+        result = camera.getLatestResult();
 
-        if(result.hasTargets()) {
-            poseEstimator.update();
+        if(result.hasTargets()){
+            currentTarget = result.getBestTarget();
+            //poseEstimator.update();
+            aprilTagiD = currentTarget.getFiducialId();
+            pose = currentTarget.getCameraToTarget();
+            //USE DATA TO DRIVE ROBOT TO CORRECT POSITION, AUTO COMMAND
+            m_drivebase.drive();
+            pose.getX();
+            pose.getY();
+            
         }
     }
 }
