e072c978d494fcc3b8c8a2d7e3ff3c0e009cf1b4
ajung26 <ajung26@csus.org>
2025-02-13 15:50:11 -0800

    Update VisionDriveCommand.java

--- src/main/java/frc/robot/commands/auto/VisionDriveCommand.java
+++ src/main/java/frc/robot/commands/auto/VisionDriveCommand.java
@@ -1,3 +1,28 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.DriveSubsystem;
+
 public class VisionDriveCommand {
     //here
+}
+
+
+
+
+public class Auto21NBlueCommand extends SequentialCommandGroup{
+    private final SwerveSubsystem m_drivebase;
+    private final ArmSubsystem m_arm;
+    private final ShooterSubsystem m_shooter;
+
+    public Auto21NBlueCommand(SwerveSubsystem drivebase, ArmSubsystem arm, ShooterSubsystem shooter) {
+        m_drivebase = drivebase;
+        m_arm = arm;
+        m_shooter = shooter;
+        addCommands(new AutoSpeakerCommand(m_shooter, m_arm).withTimeout(4.05),
+                    new WaitCommand(0),
+                    m_drivebase.getAutonomousCommand("21NBlue")
+        );
+    }
 }
