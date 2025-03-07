26035a3ec51dce96e190d42b5b0a047435f989bc
ajung26 <ajung26@csus.org>
2025-03-06 17:35:41 -0800

    YAGSL Steps 1-4 have been tested
    The drive should be inverted (Driving forward=backward). The issue is that the swerve drive is acting like tank drive.

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -25,6 +25,7 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
@@ -226,6 +227,7 @@ public class SwerveSubsystem extends SubsystemBase
                                                                             swerveDrive.getOdometryHeading().getRadians(),
                                                                             swerveDrive.getMaximumChassisVelocity()));
         });
+        
     }
 
     /**
