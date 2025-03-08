5019cf18946e2eaed5d363233a92683790bd7303
ajung26 <ajung26@csus.org>
2025-03-08 13:37:00 -0800

    Updated pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -127,7 +127,7 @@ public final class Constants
         public static final double ELEVATOR_MAX_OUTUT = 0.5;
         public static final double ELEVATOR_MIN_OUTPUT = -0.3;
         //public static final PIDController L1SHOOTER_PID = new PIDController(1, 1, 1);
-        public static final double CLOSED_LOOP_RAMP_RATE = 1.0;
+        public static final double CLOSED_LOOP_RAMP_RATE = 1000.0;
         public static final double OPEN_LOOP_RAMP_RATE = 1.0;
         //public static final double ELEVATOR_FEEDFORWARD = 1;
     }
