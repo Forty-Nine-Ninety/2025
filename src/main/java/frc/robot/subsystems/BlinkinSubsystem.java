f64fa4c767d6f91d66f2140b488c87ed4084dd28
CrystalMilan <mkhedekar28@crystal.org>
2025-04-01 16:31:01 -0700

    LED Code Done
    Just needs to be tested.

--- src/main/java/frc/robot/subsystems/BlinkinSubsystem.java
+++ src/main/java/frc/robot/subsystems/BlinkinSubsystem.java
@@ -1,17 +1,46 @@
- package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-    import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-    import java.util.HashMap;
+import java.util.Optional;
 
-    import edu.wpi.first.wpilibj.DriverStation;
-    import edu.wpi.first.wpilibj.DriverStation.Alliance;
-    import edu.wpi.first.wpilibj.motorcontrol.Spark;
-    import frc.robot.Constants;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
-    public class BlinkinSubsystem extends SubsystemBase{
-    private static Spark m_blinkin;
-    
+public class BlinkinSubsystem extends SubsystemBase {
+   private static Spark m_blinkin;
+   private static BlinkinSubsystem m_controller = null;
+   private DriverStation.Alliance m_alliance;
 
-    }
+   private BlinkinSubsystem() {
+      m_blinkin = new Spark(9);
+      Optional<Alliance> m_alliance = DriverStation.getAlliance();
+   }
+
+   public static BlinkinSubsystem getInstance() {
+      if (m_controller == null) {
+         m_controller = new BlinkinSubsystem();
+      }
+      return m_controller;
+   }
+
+   public void changeLEDColor() {
+      // Update the alliance in case it changes
+      Optional<Alliance> m_alliance = DriverStation.getAlliance();  
+      
+      if (m_alliance.isPresent()){
+         
+         if (m_alliance.get() == Alliance.Red) {
+            m_blinkin.set(0.61);  // Set red color (solid colors, red)
+
+         } else if (m_alliance.get() == Alliance.Blue) {
+            m_blinkin.set(0.85); // Set blue color (solid colors, dark blue)
+
+         } else {
+            m_blinkin.set(-0.99);  // Set default color (rainbow palette)
+         }
+      }
+   }
+}
 
